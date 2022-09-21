0e5ea45d6f7e1bfa5711ee7de80a0aea31e3ffe0
Benjamin Nesom <17NesomB@fulford.york.sch.uk>
2022-09-21 12:02:52 +0100

    Add project files.

---
+++ Scope/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("English");
+            SayHello("German");
+            SayHello("French");
+            SayHello("HAOSIUh");
+
+        }
+
+        private static void SayHello(string language)
+        {
+            switch(language)
+            {
+                case "English":
+                    Console.WriteLine("Hello");
+                    break;
+                case "German":
+                    Console.WriteLine("Guten Tag");
+                    break;
+                case "French":
+                    Console.WriteLine("Bonjour");
+                    break;
+                default:
+                    Console.WriteLine("How do?!");
+                    break;
+            }
+        }
+    }
+}
